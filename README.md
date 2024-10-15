7fb57a5ac5323c26dda7e7dff7368ff863528b6d
EChero11 <u20201f282@upc.edu.pe>
2024-10-14 20:17:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# movil_application
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/movil_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.movil_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
