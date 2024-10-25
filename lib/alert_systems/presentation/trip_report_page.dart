034e88bf849a0672a778ca9af38fbedba2ec4bfe
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-25 18:59:19 -0500

    feat: add trip report

--- lib/alert_systems/presentation/trip_report_page.dart
+++ lib/alert_systems/presentation/trip_report_page.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:movil_application/common/widgets/pressure_chart.dart';
+import 'package:movil_application/common/widgets/temperature_chart.dart';
 
+class TripReportPage extends StatelessWidget {
+  const TripReportPage({super.key});
+
+  void _sendAlert() {
+    print("¡Alerta enviada!");
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+	  );
+  }
+}
