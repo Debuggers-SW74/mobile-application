c363cbe8af886ba04584f28f9fc5a5746d5a32f2
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-25 19:04:28 -0500

    feat: add temperature graph

--- lib/alert_systems/presentation/trip_report_page.dart
+++ lib/alert_systems/presentation/trip_report_page.dart
@@ -18,7 +18,35 @@ class TripReportPage extends StatelessWidget {
         backgroundColor: Colors.blueAccent,
       ),
        body: Padding( )
-	    
+	 padding: const EdgeInsets.all(16.0), 
+         child: Column(
+	    crossAxisAlignment: CrossAxisAlignment.start,
+	    children: [
+		    Text(
+	              'Datos del Sensor',
+	              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+	            ),
+	            SizedBox(height: 20), 
+		    // Gráfico de lineas para temperatura
+	            Container(
+	              height: 300,
+	              decoration: BoxDecoration(
+	                color: Colors.white,
+	                borderRadius: BorderRadius.circular(15),
+	                boxShadow: [
+	                  BoxShadow(
+	                    color: Colors.grey.withOpacity(0.2),
+	                    spreadRadius: 5,
+	                    blurRadius: 7,
+	                    offset: Offset(0, 3),
+	                  ),
+	                ],
+	              ),
+	              child: TemperatureChart(),
+	            ),
+	            SizedBox(height: 20), 
+	    ]	 
+	 )
 	  );
   }
 }
