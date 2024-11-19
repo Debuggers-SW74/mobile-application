1d2db4436de7bbbd9eb9dc6589df95392ae4d1a0
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-11-18 23:17:22 -0500

    feat: deploy settings

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,15 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MultiProvider(
