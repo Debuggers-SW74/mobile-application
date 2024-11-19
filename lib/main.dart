61258bd2277a48059940ffaa4dc6c7dcfd410576
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-11-19 01:32:53 -0500

    fix start error

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,12 @@ import 'firebase_options.dart';
 
 void main() async {
 
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MultiProvider(
       providers: [
